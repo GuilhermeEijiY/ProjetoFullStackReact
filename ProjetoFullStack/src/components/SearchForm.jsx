1aae5a3348e8ebfdc1371ab20154a4918eaedb2a
GuilhermeEijiY <guilherme-yoshida@hotmail.com>
2025-05-07 15:27:10 -0300

    Inserção do restante dos arquivos jsx
    Inserção dos arquivos jsx que faltavam para a operação da aplicação

---
+++ ProjetoFullStack/src/components/SearchForm.jsx
@@ -0,0 +1,30 @@
+import { useForm } from 'react-hook-form';
+import { useContext } from 'react';
+import { RecipesContext } from '../contexts/RecipesContext';
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from 'yup';
+import './SearchForm.css';
+
+const schema = yup.object().shape({
+  search: yup.string().required('Campo obrigatório'),
+});
+
+export default function SearchForm() {
+  const { fetchRecipes } = useContext(RecipesContext);
+
+  const { register, handleSubmit, formState: { errors } } = useForm({
+    resolver: yupResolver(schema),
+  });
+
+  const onSubmit = (data) => {
+    fetchRecipes(data.search);
+  };
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <input {...register('search')} placeholder="Digite o nome da receita" />
+      <button type="submit">Buscar</button>
+      {errors.search && <p>{errors.search.message}</p>}
+    </form>
+  );
+}
